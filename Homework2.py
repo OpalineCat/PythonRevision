2e38470f8e951213626b6a7cf99bac96b932b9ad
OpalineCat <anushka.skhaitan@gmail.com>
2026-04-11 09:04:24 +0530

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,17 @@
+def is_armstrong(num):
+   
+    num_str = str(num)
+    n = len(num_str)
+    
+   
+    total_sum = sum(int(digit) ** n for digit in num_str)
+    
+    
+    return total_sum == num
+
+# Test the program
+number = int(input("Enter a number: "))
+if is_armstrong(number):
+    print(f"{number} is an Armstrong number.")
+else:
+    print(f"{number} is not an Armstrong number.")
